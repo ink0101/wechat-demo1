3adb7494de9228eb1eb8d4394b11f8d9db41b186
ink0101 <cftt2016@gmail.com>
2018-07-06 11:24:43 +0800

    [fix] 自定义组件

--- demo01/components/com1/com1.js
+++ demo01/components/com1/com1.js
@@ -4,20 +4,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    propA: String,
+  },
 
+  options: {
+    multipleSlots: true,
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    
   }
 })
